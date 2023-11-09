c511d9b5c2fb6cd8055c90567a74d8d4d852128a
ASUKA_RISFIN <116791022+kieutanphuoc84tv@users.noreply.github.com>
2023-11-09 07:45:32 +0700

    chu vi hình tam giác

---
+++ tam giac/Main.java
@@ -0,0 +1,8 @@
+///bài 2
+public class Main {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(10, 5);
+        System.out.println("Dien tich hinh chu nhat: " + rectangle.calculateArea());
+        System.out.println("Chu vi hinh chu nhat: " + rectangle.calculatePerimeter());
+    }
+}
