6bdfdbeee6147f9796fbf7003c98f9316b9b2d0b
ASUKA_RISFIN <116791022+kieutanphuoc84tv@users.noreply.github.com>
2023-11-16 12:39:06 +0700

    Update Employee.java

--- NhanVien/Employee.java
+++ NhanVien/Employee.java
@@ -1,26 +1,84 @@
 //bài 4
-import java.util.Date;
-
+package Employee;
+ 
+import java.util.Scanner;
+ 
 public class Employee {
-    private String lastName;
-    private Date dateOfBirth;
-    private Date startDate;
-
-    public Employee(String lastName, Date dateOfBirth, Date startDate) {
-        this.lastName = lastName;
-        this.dateOfBirth = dateOfBirth;
-        this.startDate = startDate;
+    private String maNhanVien, hoTen ;
+    private int tuoi;
+    protected int luong, luongCoBan;
+    Scanner scanner = new Scanner(System.in);
+     
+    public Employee() {
+        super();
     }
-
-    public void displayInfo() {
-        System.out.println("TEN: " + lastName);
-        System.out.println("Ngay Sinh: " + dateOfBirth);
-        System.out.println("Bat Dau Ngay Lam: " + startDate);
+ 
+    public Employee(String maNhanVien, String hoTen, String mail, int tuoi, 
+        int luong, int luongCoBan) {
+        super();
+        this.maNhanVien = maNhanVien;
+        this.hoTen = hoTen;
+        this.tuoi = tuoi;
+        this.luong = luong;
+        this.luongCoBan = luongCoBan;
     }
-
-    public static void main(String[] args) {
-    
-        Employee employee = new Employee("NguyenVanA", new Date(2023, 1, 1), new Date(2021, 1, 1));
-        employee.displayInfo();
+ 
+    public String getMaNhanVien() {
+        return maNhanVien;
+    }
+ 
+    public void setMaNhanVien(String maNhanVien) {
+        this.maNhanVien = maNhanVien;
+    }
+ 
+    public String getHoTen() {
+        return hoTen;
+    }
+ 
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+ 
+    public int getTuoi() {
+        return tuoi;
+    }
+ 
+    public void setTuoi(int tuoi) {
+        this.tuoi = tuoi;
+    }
+ 
+    public int getLuong() {
+        return luong;
+    }
+ 
+    public void setLuong(int luong) {
+        this.luong = luong;
+    }
+ 
+    public int getLuongCoBan() {
+        return luongCoBan;
+    }
+ 
+    public void setLuongCoBan(int luongCoBan) {
+        this.luongCoBan = luongCoBan;
+    }
+     
+    public void nhap() {
+        System.out.print("Nhap ma nhan vien: ");
+        maNhanVien = scanner.nextLine();
+        System.out.print("Nhap ho ten nhan vien: ");
+        hoTen = scanner.nextLine();
+        System.out.print("Nhap luong co ban: ");
+        luongCoBan = scanner.nextInt();
+    }
+     
+    public int tinhLuong() {
+        return 0;
+    }
+     
+    @Override
+    public String toString() {
+        return "ma nhan vien: " + this.maNhanVien + ", ho ten nhan vien: " + this.hoTen + 
+            ", tuoi: " + this.tuoi + ", luong co ban: " + this.luongCoBan;
     }
 }
